aeb1f4a9058f4432dd1facab29d4065fba554954
redahm <hammada.reda.dev@gmail.com>
2021-12-09 10:21:05 +0100

    function readwork finished

---
+++ prototype1/script.js
@@ -0,0 +1,31 @@
+document.getElementById("formSubmit").addEventListener("submit", function (event){
+    event.preventDefault();
+    var work = readWork();
+});
+
+
+
+function readWork(){
+
+    var work = {};
+    work["title"] = document.getElementById("inputTitle").value;
+    work["author"] = document.getElementById("inputAuthor").value;
+    work["price"] = parseFloat(document.getElementById("inputPrix").value);
+    work["date"] = document.getElementById("inputDate").value;
+    work["language"] = document.getElementById("inputLanguage").value;
+    var cheackValues = document.getElementsByName("workType");
+    for (var i = 0; i < cheackValues.length; i++) {
+        if (cheackValues[i].checked) {
+            work["type"] = cheackValues[i].value;
+            break;
+        }
+}
+
+return work;
+
+}
+
+function insertNewRow(work){
+
+    var
+}
