2d20db480ebe947417e9ccbba58375da64a5af9f
reda hammada <hammada.reda.dev@gmail.com>
2021-12-11 18:48:18 +0100

    update prototype

--- prototype2/script.js
+++ prototype2/script.js
@@ -1,69 +1,46 @@
-document.getElementById("formSubmit").addEventListener("submit", function (event){
+document.getElementById('formSubmit').addEventListener("submit", function(event){
     event.preventDefault();
-    var work = readWork();
-    insertNewRow(work)
+    var work = workData();
+    insertNewWork = work;
+
 });
 
-
-
-function readWork(){
+function workData(){
 
     var work = {};
-    work["title"] = document.getElementById("inputTitle").value;
-    work["author"] = document.getElementById("inputAuthor").value;
+    work["Title"] = document.getElementById('inputTitle').value;
+    work["Author"] = document.getElementById('inputAuthor').value;
     work["price"] = parseFloat(document.getElementById("inputPrix").value);
-    work["date"] = document.getElementById("inputDate").value;
-    work["language"] = document.getElementById("inputLanguage").value;
-    var cheackValues = document.getElementsByName("workType");
-    for (var i = 0; i < cheackValues.length; i++) {
-        if (cheackValues[i].checked) {
-            work["type"] = cheackValues[i].value;
+    work["Date"] = document.getElementById('inputDate').value;
+    work["language"] = document.getElementById('inputLanguage').value;
+    var checkedvalues = document.getElementsByName("workType");
+    for( i = 0; i < checkedvalues.length; i++){
+        if(checkedvalues[i].checked){
+            work["type"] = checkedvalues[i];
             break;
         }
-}
-return work;
-
-}
-
-
-function insertNewRow(work) {
-    var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
-    var newRow = tableBody.insertRow(tableBody.length);
-    newRow.insertCell(0).innerHTML = work.title;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = work.author;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = work.price;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = work.date;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = work.language
-    cell6 = newRow.insertCell(5)
-    cell6.innerHTML = work.type
-    cell7 = newRow.insertCell(6)
-    cell7.innerHTML = "<button onclick ='Edit(this)'>Update</button> <button onclick='Delete(this)'>Delete</button>";
-
-
- 
-
-}
-
-function Edit(){
-
-
-}
-
-function Delete(td){
-    if(confirm("Are you sure you want to delete this")){
-        
-        row = td.parentElement.parentElement;
-        document.getElementById("worksTable").deleteRow(row.rowIndex);
-
-
     }
-
-
+    return work;
 }
 
+function insertNewWork(work){
+
+    var tableBody = document.getElementsByTagName('tbody')[0];
+    var row = tableBody.insertRow(tableBody.length);
+    var cell = row.insertCell(0).innerHTML = work['title'];
+    cell = row.insertCell(1);
+    cell.innerHTML = work.author;
+    cell = row.insertCell(2);
+    cell.innerHTML = work.price;
+    cell = row.insertCell(3);
+    cell.innerHTML = work.date;
+    cell = row.insertCell(4);
+    cell.innerHTML = work.language
+    cell = row.insertCell(5)
+    cell.innerHTML = work.type
+    cell = row.insertCell(6)
 
 
+
+    
+}
